2a25d9a286f81e92a8f3601fff3676b24e02ca02
Ben <ben@Bens-MacBook-Air.local>
2017-07-27 12:08:38 -0400

    initial

---
+++ app.js
@@ -0,0 +1,44 @@
+var express = require("express"),
+	app = express(),
+	bodyParser = require("body-parser"),
+	mongoose = require("mongoose");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.use(express.static("partials"));
+
+app.set("view engine","ejs");
+
+// add DB
+
+mongoose.connect("mongodb://localhost/list")
+
+// Add model/schema for todo - 1 string
+
+var listSchema = new mongoose.Schema({
+	do: String
+});
+
+var List = mongoose.model("List",listSchema);
+
+// make initial route
+
+app.get("/",function(req,res){
+	List.find({},function(err,foundList){
+		if(err){
+			console.log(err)
+			res.send("Sorry, but we have encountered an error: " + err)
+		} else {
+			res.render("main",{lists:foundList})
+		}
+	});
+});
+
+app.post("/",function(req,res){
+	List.create({
+		do: req.body.content
+	},function(){res.redirect('/')});
+})
+
+// listen
+app.listen(3000)
